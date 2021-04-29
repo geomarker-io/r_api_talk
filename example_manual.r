df3ee2b048e6c3384bbe189afe1002238c6e5abb
Andrew Vancil <andrew.vancil@cchmc.org>
2021-04-29 14:44:09 -0400

    images for examples

--- example_manual.r
+++ example_manual.r
@@ -10,4 +10,30 @@ df <- df %>%
   filter(!is.na(features.attributes.NEWADDR)) %>%
   select(address = features.attributes.NEWADDR, lon = features.geometry.x, lat = features.geometry.y)
 df <- sf::st_as_sf(df, coords = c('lon', 'lat'), crs = 4326)
-mapview::mapview(df)
+
+mapview::mapviewOptions(fgb = F)
+ex_map<-mapview::mapview(df)
+htmlwidgets::saveWidget(ex_map@map, file = "ex_map.html")
+
+mapview::mapshot(ex_map, file = "ex_map.png")
+options(scipen = 99)
+
+remotes::install_github("r-spatial/mapview")
+
+library(tmap)
+tmap_mode("view")
+
+ex_map<-tm_basemap("CartoDB.Positron") +
+  tm_shape(df)+
+  tm_dots(col="address", palette = "viridis", size = .08)
+ex_map
+
+tmap_save(ex_map, "ex_map.png")
+
+ex_map %>%
+  tmap_leaflet() %>%
+  htmlwidgets::saveWidget(file = "ex_map.html")
+
+#https://data-cagisportal.opendata.arcgis.com/datasets/cincinnati-police-departments/geoservice?selectedAttribute=ADDRESS
+
+
